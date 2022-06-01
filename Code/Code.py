447586cedd48c2e3d545510a684b01bfe0335b32
Mohammad mahdi Soori <mohammadmahdisoori@gmail.com>
2022-06-01 13:08:35 +0430

    complete statistic

--- Code/Code.py
+++ Code/Code.py
@@ -1,27 +1,100 @@
+import os
+import threading
 import subprocess
 from time import sleep
+from optparse import OptionParser
 from mininet.net import Mininet
 from mininet.node import Host, Switch
 # from mininet.cli import CLI
 from mininet.link import TCLink
 from mininet.log import setLogLevel
 from mininet.topo import Topo
-import os
 
-def write_sysctl(key, value):
-    """ Write kernel parameters. """
-    subprocess.check_output('sysctl -w {}={}'.format(key, value), shell=True)
 
-def setup_mptcp(net):
-    hosts = [v for v in net.values() if isinstance(v, Host)]
-    switches = [v for v in net.values() if isinstance(v, Switch)]
-    for switch_i, switch in enumerate(switches, start=1):
-        for host_i, host in enumerate(hosts, start=1):
-            # connectionsTo returns list of (host-interface, switch-interface)
-            # we want the host interface connected to the switch
-            interface, _ = host.connectionsTo(switch)[0]
-            interface.setIP('10.0.{}.{}/24'.format(switch_i, host_i))
+# def setup_mptcp(net):
+#     hosts = [v for v in net.values() if isinstance(v, Host)]
+#     switches = [v for v in net.values() if isinstance(v, Switch)]
+#     for switch_i, switch in enumerate(switches, start=1):
+#         for host_i, host in enumerate(hosts, start=1):
+#             # connectionsTo returns list of (host-interface, switch-interface)
+#             # we want the host interface connected to the switch
+#             interface, _ = host.connectionsTo(switch)[0]
+#             interface.setIP('10.0.{}.{}/24'.format(switch_i, host_i))
 
+# mode 1 : congestionControl = lia, pathManager = default
+# mode 2 : congestionControl = olia, pathManager = default
+# mode 3 : congestionControl = wvegas, pathManager = default
+# mode 4 : congestionControl = balia, pathManager = default
+# mode 5 : congestionControl = lia, pathManager = fullmesh
+# mode 6 : congestionControl = olia, pathManager = fullmesh
+# mode 7 : congestionControl = wvegas, pathManager = fullmesh
+# mode 8 : congestionControl = balia, pathManager = fullmesh
+# mode 9 : congestionControl = lia, pathManager = ndiffports
+# mode 10 : congestionControl = olia, pathManager = ndiffports
+# mode 11 : congestionControl = wvegas, pathManager = ndiffports
+# mode 12 : congestionControl = balia, pathManager = ndiffports
+# mode 13 : congestionControl = lia, pathManager = binder
+# mode 14 : congestionControl = olia, pathManager = binder
+# mode 15 : congestionControl = wvegas, pathManager = binder
+# mode 16 : congestionControl = balia, pathManager = binder
+def setupCongestionControll(mode=1):
+    subprocess.check_output('sudo sysctl -w {}={}'.format("net.mptcp.mptcp_enabled", 1), shell=True)
+    if mode == 1:
+        os.system('sudo modprobe mptcp_coupled && sudo sysctl -w net.ipv4.tcp_congestion_control=lia ')
+        os.system('sudo sysctl -w net.mptcp.mptcp_path_manager=default ')
+    elif mode == 2:
+        os.system('sudo modprobe mptcp_olia && sudo sysctl -w net.ipv4.tcp_congestion_control=olia ')
+        os.system('sudo sysctl -w net.mptcp.mptcp_path_manager=default ')
+    elif mode == 3:
+        os.system('sudo modprobe mptcp_wvegas && sudo sysctl -w net.ipv4.tcp_congestion_control=wvegas ')
+        os.system('sudo sysctl -w net.mptcp.mptcp_path_manager=default ')
+    elif mode == 4:
+        os.system('sudo modprobe mptcp_balia && sudo sysctl -w net.ipv4.tcp_congestion_control=balia ')
+        os.system('sudo sysctl -w net.mptcp.mptcp_path_manager=default ')
+    elif mode == 5:
+        os.system('sudo modprobe mptcp_coupled && sudo sysctl -w net.ipv4.tcp_congestion_control=lia ')
+        os.system('sudo sysctl -w net.mptcp.mptcp_path_manager=fullmesh ')
+        os.system('echo 1 | sudo tee /sys/module/mptcp_fullmesh/parameters/num_subflows')
+    elif mode == 6:
+        os.system('sudo modprobe mptcp_olia && sudo sysctl -w net.ipv4.tcp_congestion_control=olia ')
+        os.system('sudo sysctl -w net.mptcp.mptcp_path_manager=fullmesh ')
+        os.system('echo 1 | sudo tee /sys/module/mptcp_fullmesh/parameters/num_subflows')
+    elif mode == 7:
+        os.system('sudo modprobe mptcp_wvegas && sudo sysctl -w net.ipv4.tcp_congestion_control=wvegas ')
+        os.system('sudo sysctl -w net.mptcp.mptcp_path_manager=fullmesh ')
+        os.system('echo 1 | sudo tee /sys/module/mptcp_fullmesh/parameters/num_subflows')
+    elif mode == 8:
+        os.system('sudo modprobe mptcp_balia && sudo sysctl -w net.ipv4.tcp_congestion_control=balia ')
+        os.system('sudo sysctl -w net.mptcp.mptcp_path_manager=fullmesh ')
+        os.system('echo 1 | sudo tee /sys/module/mptcp_fullmesh/parameters/num_subflows')
+    elif mode == 9:
+        os.system('sudo modprobe mptcp_coupled && sudo sysctl -w net.ipv4.tcp_congestion_control=lia ')
+        os.system('sudo modprobe mptcp_ndiffports && sudo sysctl -w net.mptcp.mptcp_path_manager=ndiffports ')
+        os.system('echo 1 | sudo tee /sys/module/mptcp_ndiffports/parameters/num_subflows')
+    elif mode == 10:
+        os.system('sudo modprobe mptcp_olia && sudo sysctl -w net.ipv4.tcp_congestion_control=olia ')
+        os.system('sudo modprobe mptcp_ndiffports && sudo sysctl -w net.mptcp.mptcp_path_manager=ndiffports ')
+        os.system('echo 1 | sudo tee /sys/module/mptcp_ndiffports/parameters/num_subflows')
+    elif mode == 11:
+        os.system('sudo modprobe mptcp_wvegas && sudo sysctl -w net.ipv4.tcp_congestion_control=wvegas ')
+        os.system('sudo modprobe mptcp_ndiffports && sudo sysctl -w net.mptcp.mptcp_path_manager=ndiffports ')
+        os.system('echo 1 | sudo tee /sys/module/mptcp_ndiffports/parameters/num_subflows')
+    elif mode == 12:
+        os.system('sudo modprobe mptcp_balia && sudo sysctl -w net.ipv4.tcp_congestion_control=balia ')
+        os.system('sudo modprobe mptcp_ndiffports && sudo sysctl -w net.mptcp.mptcp_path_manager=ndiffports ')
+        os.system('echo 1 | sudo tee /sys/module/mptcp_ndiffports/parameters/num_subflows')
+    elif mode == 13:
+        os.system('sudo modprobe mptcp_coupled && sudo sysctl -w net.ipv4.tcp_congestion_control=lia ')
+        os.system('sudo modprobe mptcp_binder && sudo sysctl -w net.mptcp.mptcp_path_manager=binder ')
+    elif mode == 14:
+        os.system('sudo modprobe mptcp_olia && sudo sysctl -w net.ipv4.tcp_congestion_control=olia ')
+        os.system('sudo modprobe mptcp_binder && sudo sysctl -w net.mptcp.mptcp_path_manager=binder ')
+    elif mode == 15:
+        os.system('sudo modprobe mptcp_wvegas && sudo sysctl -w net.ipv4.tcp_congestion_control=wvegas ')
+        os.system('sudo modprobe mptcp_binder && sudo sysctl -w net.mptcp.mptcp_path_manager=binder ')
+    elif mode == 16:
+        os.system('sudo modprobe mptcp_balia && sudo sysctl -w net.ipv4.tcp_congestion_control=balia ')
+        os.system('sudo modprobe mptcp_binder && sudo sysctl -w net.mptcp.mptcp_path_manager=binder ')
 
 ### CONFIGURATIONS ###
 
@@ -40,11 +113,11 @@ def setup_mptcp(net):
 # "path-managers:"
 
 #os.system('sysctl -w net.mptcp.mptcp_path_manager=default ')
-#os.system('sysctl -w net.mptcp.mptcp_path_manager=fullmesh ')
-#os.system('echo 1 | sudo tee /sys/module/mptcp_fullmesh/parameters/num_subflows')
+# os.system('sysctl -w net.mptcp.mptcp_path_manager=fullmesh ')
+# os.system('echo 1 | sudo tee /sys/module/mptcp_fullmesh/parameters/num_subflows')
 #os.system('modprobe mptcp_ndiffports && sysctl -w net.mptcp.mptcp_path_manager=ndiffports ')
 #os.system('echo 1 | sudo tee /sys/module/mptcp_ndiffports/parameters/num_subflows')
-#os.system('modprobe mptcp_binder && sysctl -w net.mptcp.mptcp_path_manager=binder ')
+# os.system('modprobe mptcp_binder && sysctl -w net.mptcp.mptcp_path_manager=binder ')
 
 # "scheduler:"
 
@@ -115,71 +188,59 @@ class MPTopo(Topo):
     #     self.addLink(s1, s2, bw=10, delay=self.Adelay) #  delay='5ms'
     #     self.addLink(s3, s4, bw=20, delay=self.Bdelay)
 
+def mptcpConnection(h1,h2,file):
+    h1String = h1.cmd('iperf -c ' + h2.IP() + " -t 50 ")
+    print(h1String)
+    file.write(h1String)
+    # str=h1.cmd('iperf -c', h2.IP(), '-t 50')
+    # file.write(str)
+def tcpConnection(h3,h4,file):
+    time.sleep(10.0)
+    h3String = h3.cmd('iperf -c ' + h4.IP() + " -t 20 ")
+    print(h3String)
+    file.write(h3String)
+    # print(h3.cmd('iperf -c', h4.IP(), '-Z cubic -t 20'))
 
 if '__main__' == __name__:
-    setLogLevel('info')
-    net = Mininet(topo=MPTopo(Adelay="20ms",Bdelay="20ms"), link=TCLink)
-    write_sysctl('net.mptcp.mptcp_enabled', 1)
+    parser = OptionParser()
+    parser.add_option("--bdelay", dest="bdelay", default="20ms")
+    (options, args) = parser.parse_args()
 
-    # setup_mptcp(net)
-    net.start()
+    h2file = open('resultH2-' + options.bdelay + '.txt', 'a')
+    h4file = open('resultH4-' + options.bdelay + '.txt', 'a')
 
-    h1 = net.get('h1')
-    h2 = net.get('h2')
-    h3 = net.get('h3')
-    h4 = net.get('h4')
 
-    # result = h1.cmd('iperf h2')
-    # print(result)
-    # net.iperf((h1, h2))
+    for i in range(1): #20
+        print("Starting test number :" + str(i+1))
+        setLogLevel('info')
+        net = Mininet(topo=MPTopo(Adelay="20ms",Bdelay=options.bdelay), link=TCLink)
+        setupCongestionControll()
+        net.start()
+        h1 = net.get('h1')
+        h2 = net.get('h2')
+        h3 = net.get('h3')
+        h4 = net.get('h4')
+        sleep(3)
+        print(h2.cmd('iperf -s &'))
+        print(h4.cmd('iperf -s &'))
 
-    print("Starting test...")
-    # h1.cmd('while true; do iperf h2; sleep 1; &') # done > /tmp/iperfh1h2.out
-    print(h2.cmd('iperf -s &'))
-    # h1.cmd('iperf', '-c', h2.IP() , '-t', '10')
-    print(h1.cmd('iperf -c ' + h2.IP() + " -t 50 > result &"))
-    # h2.cmdPrint('iperf -c 10.0.1.1 -i 1')
-    # net.iperf((h1, h2))
-    sleep(10)
-    # h3.cmd('while true; do iperf h4; sleep 1; &')
-    print(h4.cmd('iperf -s &'))
-    # h1.cmd('iperf', '-c', h2.IP() , '-t', '10')
-    print(h3.cmd('iperf -c ' + h4.IP() + " -t 20 > result2 &"))
-    # net.iperf((h3, h4))
-    # sleep(20)
-    # h3.cmd('kill %while')
-    # sleep(20)
-    sleep(40)
-    # h1.cmd('kill %while')
-    print("Stopping test")
-    # print("Reading output")
-    # f = open('/tmp/iperfh1h2.out')
-    # lineno = 1
-    # for line in f.readlines():
-    #     print("%d: %s" % (lineno, line.strip()))
-    #     lineno += 1
-    # f.close()
+        # h1String = h1.cmd('iperf -c ' + h2.IP() + " -t 50 &")
+        # print(h1String)
+        # h2file.write(h1String)
+        # sleep(10)
+        #
+        # print(h3.cmd('iperf -c ' + h4.IP() + " -t 20 > result2 &"))
+        # sleep(40)
+        t1 = threading.Thread(target=mptcpConnection, args=(h1,h2,h2file))
+        t2 = threading.Thread(target=tcpConnection, args=(h3,h4,h4file))
 
-    # print("Starting test...")
-    # h3.cmd('while true; do iperf h4; sleep 1; done > /tmp/iperfh3h4.out &')
-    # sleep(10)
-    # print("Stopping test")
-    # h3.cmd('kill %while')
-    # print("Reading output")
-    # f = open('/tmp/iperfh1h2.out')
-    # lineno = 1
-    # for line in f.readlines():
-    #     print("%d: %s" % (lineno, line.strip()))
-    #     lineno += 1
-    # f.close()
+        t1.start()
+        t2.start()
 
-    # We need to give mptcp some time
-    # print('=========================================================')
-    # print('Set up two paths with 5 MBit each. Running iperf in 3s...')
-    # print('=========================================================')
-    # sleep(3)
-    # CLI(net)
-    # h1, h2, h3, h4 = [net.get(n) for n in hostnames]
-    # h1.cmd('iperf -s &')
-    # h2.cmdPrint('iperf -c 10.0.1.1 -i 1')
-    net.stop()
+        t1.join()
+        t2.join()
+
+        h2.cmd('kill %while')
+        h4.cmd('kill %while')
+        print("Stopping test number:" + str(i+1))
+        net.stop()
--- Code/statistic.py
+++ Code/statistic.py
@@ -1,15 +1,45 @@
 import statistics
 import math
+import re
 import matplotlib.pyplot as plt
+
+def getDataFromFile(fileAddress,expectedNumber):
+    with open(fileAddress, 'r') as file:
+        fileText = file.read()
+    indexes = [m.start() for m in re.finditer('Mbits/sec', fileText)]
+    if len(indexes) != expectedNumber:
+        print("FALSE")
+        return []
+    data = []
+    for i in range(len(indexes)):
+        data.append(float(data[indexes[i] - 7:indexes[i] - 1]))
+    return data
 
 def getErrorBarData(data,t = 2.009): # for 50 if we calculate 20 times t is 2.086
     standardDeviation = statistics.stdev(data)
     squareData = math.sqrt(len(data))
     return t * (standardDeviation / squareData)
 
-def draw(data):
-    confidence = getErrorBarData(data)
-    dataMean = statistics.mean(data)
-    plt.errorbar([1], dataMean, yerr=confidence)
+def draw(dataNdimension):
+    means = []
+    confidences = []
+    for i in range(dataNdimension):
+        confidence = getErrorBarData(dataNdimension[i])
+        dataMean = statistics.mean(dataNdimension[i])
+    plt.errorbar(range(len(dataNdimension)), means, yerr=confidences)
     plt.title('errorbar function Example')
     plt.show()
+
+def getDataNdimension(fileAddressList,expectedNumber):
+    dataNdimension = []
+    for i in range(len(fileAddressList)):
+        oneData = getDataFromFile(fileAddressList[i],expectedNumber)
+        if len(oneData) == 0:
+            print("FALSE FALSE")
+            return
+        dataNdimension.append(oneData)
+    return dataNdimension
+    
+fileAddressList = ['resultH2-20ms.txt']
+dataNdimension = getDataNdimension(fileAddressList,1)
+draw(dataNdimension)
