e1d158fc1728b00c70bc347acaf953fb5435db14
Mohammad mahdi Soori <mohammadmahdisoori@gmail.com>
2022-06-04 15:59:33 +0430

    create pictures

--- Code/statistic.py
+++ Code/statistic.py
@@ -3,19 +3,22 @@ import math
 import re
 import matplotlib.pyplot as plt
 
-def getDataFromFile(fileAddress,expectedNumber):
+def getDataFromFile(fileAddress,expectedNumber,mode=1):
     with open(fileAddress, 'r') as file:
         fileText = file.read()
-    indexes = [m.start() for m in re.finditer('Mbits/sec', fileText)]
+    if mode == 1:
+        indexes = [m.start() for m in re.finditer('Mbits/sec', fileText)]
+    else:
+        indexes = [m.start() for m in re.finditer('MBytes', fileText)]
     if len(indexes) != expectedNumber:
         print("FALSE")
         return []
     data = []
     for i in range(len(indexes)):
-        data.append(float(data[indexes[i] - 7:indexes[i] - 1]))
+        data.append(float(fileText[indexes[i] - 7:indexes[i] - 1]))
     return data
 
-def getErrorBarData(data,t = 2.009): # for 50 if we calculate 20 times t is 2.086
+def getErrorBarData(data,t = 2.086): # for 50 is 2.009 if we calculate 20 times t is 2.086
     standardDeviation = statistics.stdev(data)
     squareData = math.sqrt(len(data))
     return t * (standardDeviation / squareData)
@@ -23,23 +26,31 @@ def getErrorBarData(data,t = 2.009): # for 50 if we calculate 20 times t is 2.08
 def draw(dataNdimension):
     means = []
     confidences = []
-    for i in range(dataNdimension):
+    for i in range(len(dataNdimension)):
         confidence = getErrorBarData(dataNdimension[i])
         dataMean = statistics.mean(dataNdimension[i])
-    plt.errorbar(range(len(dataNdimension)), means, yerr=confidences)
-    plt.title('errorbar function Example')
+        means.append(dataMean)
+        confidences.append(confidence)
+    # print(range(len(dataNdimension)))
+    print(means)
+    print(confidences)
+    plt.errorbar([20,40,60,80,100], means, yerr=confidences)
+    plt.title('Bandwidth of lia and fullmesh')
+    plt.xlabel("B link delay")
+    plt.ylabel("Bandwidth of H3-H4")
     plt.show()
 
 def getDataNdimension(fileAddressList,expectedNumber):
     dataNdimension = []
     for i in range(len(fileAddressList)):
-        oneData = getDataFromFile(fileAddressList[i],expectedNumber)
+        oneData = getDataFromFile(fileAddressList[i],expectedNumber,1)
         if len(oneData) == 0:
             print("FALSE FALSE")
             return
         dataNdimension.append(oneData)
     return dataNdimension
     
-fileAddressList = ['resultH2-20ms.txt']
-dataNdimension = getDataNdimension(fileAddressList,1)
+fileAddressList = ['resultH4-20ms-mode5.txt','resultH4-40ms-mode5.txt','resultH4-60ms-mode5.txt','resultH4-80ms-mode5.txt','resultH4-100ms-mode5.txt']
+dataNdimension = getDataNdimension(fileAddressList,20)
+# print(dataNdimension)
 draw(dataNdimension)
