91403c20e34a80b665e274eb95015c6934f78a44
Mohammad mahdi Soori <mohammadmahdisoori@gmail.com>
2022-06-05 16:33:10 +0430

    add pdf for project

--- Code/Code.py
+++ Code/Code.py
@@ -5,21 +5,9 @@ from time import sleep
 from optparse import OptionParser
 from mininet.net import Mininet
 from mininet.node import Host, Switch
-# from mininet.cli import CLI
 from mininet.link import TCLink
 from mininet.log import setLogLevel
 from mininet.topo import Topo
-
-
-# def setup_mptcp(net):
-#     hosts = [v for v in net.values() if isinstance(v, Host)]
-#     switches = [v for v in net.values() if isinstance(v, Switch)]
-#     for switch_i, switch in enumerate(switches, start=1):
-#         for host_i, host in enumerate(hosts, start=1):
-#             # connectionsTo returns list of (host-interface, switch-interface)
-#             # we want the host interface connected to the switch
-#             interface, _ = host.connectionsTo(switch)[0]
-#             interface.setIP('10.0.{}.{}/24'.format(switch_i, host_i))
 
 # mode 1 : congestionControl = lia, pathManager = default
 # mode 2 : congestionControl = olia, pathManager = default
@@ -128,16 +116,6 @@ def setupCongestionControll(mode=1):
 #os.system('modprobe mptcp_redundant && sysctl -w net.mptcp.mptcp_scheduler=redundant ')
 
 class MPTopo(Topo):
-    # def build(self, hostnames, number_of_paths=2, bw=5):
-    #     linkopt = dict(bw=bw)
-    #
-    #     for h in hostnames:
-    #         self.addHost(h)
-    #
-    #     for i_p in range(0, number_of_paths):
-    #         s = self.addSwitch('s{}'.format(i_p))
-    #         for h in self.hosts():
-    #             self.addLink(h, s, **linkopt)
     def __init__(self, Adelay, Bdelay):
         Topo.__init__(self)
         self.Adelay = Adelay
@@ -165,41 +143,15 @@ class MPTopo(Topo):
         self.addLink(s1, s2, bw=10, delay=self.Adelay)  # delay='5ms'
         self.addLink(s3, s4, bw=20, delay=self.Bdelay)
 
-    # def build(self):
-    #     h1 = self.addHost('h1')
-    #     h2 = self.addHost('h2')
-    #     h3 = self.addHost('h3')
-    #     h4 = self.addHost('h4')
-    #
-    #     s1 = self.addSwitch('s1')
-    #     s2 = self.addSwitch('s2')
-    #     s3 = self.addSwitch('s3')
-    #     s4 = self.addSwitch('s4')
-    #
-    #     hostSwitchLinkSpec = dict(bw=100, delay='0.1ms')
-    #     # (or you can use brace syntax: linkopts = {'bw':10, 'delay':'5ms', ... } )
-    #     self.addLink(h1, s1, **hostSwitchLinkSpec)
-    #     self.addLink(h1, s3, **hostSwitchLinkSpec)
-    #     self.addLink(h2, s2, **hostSwitchLinkSpec)
-    #     self.addLink(h2, s4, **hostSwitchLinkSpec)
-    #     self.addLink(h3, s3, **hostSwitchLinkSpec)
-    #     self.addLink(h4, s4, **hostSwitchLinkSpec)
-    #
-    #     self.addLink(s1, s2, bw=10, delay=self.Adelay) #  delay='5ms'
-    #     self.addLink(s3, s4, bw=20, delay=self.Bdelay)
-
 def mptcpConnection(h1,h2,file):
     h1String = h1.cmd('iperf -c ' + h2.IP() + " -t 50 ")
     print(h1String)
     file.write(h1String)
-    # str=h1.cmd('iperf -c', h2.IP(), '-t 50')
-    # file.write(str)
 def tcpConnection(h3,h4,file):
     sleep(10.0)
     h3String = h3.cmd('iperf -c ' + h4.IP() + " -t 20 ")
     print(h3String)
     file.write(h3String)
-    # print(h3.cmd('iperf -c', h4.IP(), '-Z cubic -t 20'))
 
 if '__main__' == __name__:
     parser = OptionParser()
@@ -225,13 +177,6 @@ if '__main__' == __name__:
         print(h2.cmd('iperf -s &'))
         print(h4.cmd('iperf -s &'))
 
-        # h1String = h1.cmd('iperf -c ' + h2.IP() + " -t 50 &")
-        # print(h1String)
-        # h2file.write(h1String)
-        # sleep(10)
-        #
-        # print(h3.cmd('iperf -c ' + h4.IP() + " -t 20 > result2 &"))
-        # sleep(40)
         t1 = threading.Thread(target=mptcpConnection, args=(h1,h2,h2file))
         t2 = threading.Thread(target=tcpConnection, args=(h3,h4,h4file))
 
